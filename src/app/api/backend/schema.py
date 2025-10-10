125e58ed6a351fd7aa1a12496bfb99f75e56f431
Rafael Eberhardt Sarate <rafael.sarate@objectedge.com>
2025-10-10 14:22:59 -0300

    Implement medium size database and update backend

--- src/app/api/backend/schema.py
+++ src/app/api/backend/schema.py
@@ -1,6 +1,13 @@
 # contract between frontend and backend
 import graphene
-from db import products
+from sqlalchemy import create_engine, text
+
+engine = create_engine("sqlite:///products.db", echo=True)
+
+class VariantType(graphene.ObjectType):
+    color = graphene.String()
+    size = graphene.String()
+    stock = graphene.Int()
 
 # This class is but what I would call an object in JS
 class ProductType(graphene.ObjectType):
@@ -10,7 +17,7 @@ class ProductType(graphene.ObjectType):
     type = graphene.String()
     price = graphene.Float()
     size = graphene.String()
-    inventory = graphene.Int()
+    variants = graphene.List(VariantType),
     customProperties = graphene.String()
 
 class Query(graphene.ObjectType):
@@ -18,7 +25,7 @@ class Query(graphene.ObjectType):
     goodbye = graphene.String()
     all_products = graphene.List(ProductType)
     product_by_id = graphene.Field(ProductType, id=graphene.Int(required=True))
-    product_by_category = graphene.List(ProductType, category=graphene.String(required=True))
+    product_by_category = graphene.List(ProductType, category=graphene.String(required=False))
 
     #The resolve method is proper of the graphene library
     def resolve_hello(root, info, first_name):
@@ -28,18 +35,28 @@ class Query(graphene.ObjectType):
         return 'See ya!'
 
     def resolve_all_products(root, info):
-        return products
+        query = "SELECT * FROM products"
+        with engine.connect() as conn:
+            result = conn.execute(text(query))
+            return [dict(row) for row in result.mappings()]
 
     def resolve_product_by_id(root, info, id):
-        for p in products:
-            if p["id"] == id:
-                return p
-        return None
+        query = "SELECT * FROM products WHERE id = :id"
+
+        with engine.connect() as conn:
+            result = conn.execute(text(query), {"id": id})
+            row = result.mappings().fetchone()
+            return dict(row) if row else None
     
-    def resolve_product_by_category(root, info, category):
-        list_by_category = [p for p in products if p["category"] == category]
-        return list_by_category or None
+    def resolve_product_by_category(root, info, category=None):
+        #if category is not provided, return all
+        query = f"SELECT * FROM products{' WHERE category = :category' if category else ''}"
+        params = {"category": category} if category else {}
+
+        with engine.connect() as conn:
+            result = conn.execute(text(query), params)
+            products_list = [dict(row) for row in result.mappings()]
+
+        return products_list
 
 schema = graphene.Schema(query=Query)
-
-
